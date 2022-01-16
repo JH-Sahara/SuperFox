9504c1a50e0a348498517ebc5fae4a8483e40dd1
Sahara <72747324+JH-Sahara@users.noreply.github.com>
2022-01-16 19:21:31 +0800

    Animation

--- SuperFox/Assets/Scripts/Player.cs
+++ SuperFox/Assets/Scripts/Player.cs
@@ -9,6 +9,7 @@ public class Player : MonoBehaviour
     public LayerMask ground;
     public int speed = 10; //初始速度
     public int jumpForce = 50; //初始力
+    public int score = 0; //吃掉食物的得分
     // Start is called before the first frame update
     void Start()
     {
@@ -19,13 +20,13 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+         ChangeAnimation();
+        Change();
     }
 
     void FixedUpdate()
     {
         Move();
-        ChangeAnimation();
     }
 
     //角色的移动函数
@@ -50,12 +51,13 @@ public class Player : MonoBehaviour
         }
 
         //判断是否跳跃
-        if (Input.GetButton("Jump"))
+        if (Input.GetButton("Jump") && playerAnimator.GetBool("idling"))
         {
             playerAnimator.SetBool("idling",false);
             playerAnimator.SetBool("jumping",true);
             playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x,jumpForce*Time.fixedDeltaTime);
         }
+        
     }
 
     //角色移动等动画的更新
@@ -71,6 +73,26 @@ public class Player : MonoBehaviour
         }else if (playerRigidbody.IsTouchingLayers(ground) && playerRigidbody.velocity.y<=0){
             playerAnimator.SetBool("falling",false);
             playerAnimator.SetBool("idling",true);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Food")
+        {
+            Destroy(other.gameObject);
+            score ++;
+        }
+    }
+
+    //一般性输入改变状态的函数
+    private void Change()
+    {
+        //判断是否下蹲
+        if (Input.GetKeyDown(KeyCode.C) && playerAnimator.GetBool("idling"))
+        {
+            bool isCrouching = playerAnimator.GetBool("crouching");
+            playerAnimator.SetBool("crouching",!isCrouching);
+            gameObject.GetComponent<BoxCollider2D>().enabled = isCrouching;
         }
     }
 }
