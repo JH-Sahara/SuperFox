de7b2f5132780865177c6a87a9e0df4a16a43cdb
Sahara <72747324+JH-Sahara@users.noreply.github.com>
2022-01-18 12:11:57 +0800

    Panel and Dialog

---
+++ SuperFox/Assets/Scripts/EnterDialog.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterDialog : MonoBehaviour
+{
+    public GameObject enterDialog;
+    public Animator ani;
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player")
+        {
+            enterDialog.SetActive(false);
+            enterDialog.SetActive(true);
+            ani.SetBool("exiting",false);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.tag == "Player")
+        {
+            ani.SetBool("exiting",true);
+        }
+    }
+}
